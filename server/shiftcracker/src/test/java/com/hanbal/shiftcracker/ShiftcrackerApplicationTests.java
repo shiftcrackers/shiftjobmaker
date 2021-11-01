acde341fc2caaace985ebb9f06a34526358d0830
YS.Bae <qodbtn41@naver.com>
2021-11-01 14:08:04 +0000

    스프링 서버 프로젝트 생성

---
+++ server/shiftcracker/src/test/java/com/hanbal/shiftcracker/ShiftcrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanbal.shiftcracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShiftcrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
