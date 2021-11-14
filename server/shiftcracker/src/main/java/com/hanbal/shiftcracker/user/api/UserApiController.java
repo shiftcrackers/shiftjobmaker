d38a9e0a5477f46b5a97c7feed4056822078d321
qodbtn41 <qodbtn41@naver.com>
2021-11-14 21:36:00 +0900

    간호사 테이블 CRUD JPA 개발

--- server/shiftcracker/src/main/java/com/hanbal/shiftcracker/user/api/UserApiController.java
+++ server/shiftcracker/src/main/java/com/hanbal/shiftcracker/user/api/UserApiController.java
@@ -7,33 +7,61 @@ import com.hanbal.shiftcracker.global.domain.User;
 import com.hanbal.shiftcracker.global.dto.UserDto;
 import com.hanbal.shiftcracker.user.service.UserService;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
 @RestController
 @RequiredArgsConstructor
 public class UserApiController {
   private final UserService userService;
 
   @GetMapping("api/v1/users")
-  public List<UserDto> getUsers() {
+  public Result<UserDto> getUsers() {
     List<User> users = userService.findUsers();
-    return users.stream().map(user -> new UserDto(user)).collect(Collectors.toList());
+    List<UserDto> userDtos = users.stream().map(user -> new UserDto(user)).collect(Collectors.toList());
+
+    return new Result<UserDto>(userDtos);
   }
 
   @PostMapping("api/v1/users")
-  public CreateUserResponse saveUser(@RequestBody User user) {
+  public CreateUserResponse saveUser(@RequestBody CreateMemberRequest request) {
+    User user = new User(request.getName());
+
     Long id = userService.join(user);
     return new CreateUserResponse(id);
+  }
+
+  @PutMapping("api/v1/users/{id}")
+  public UpdateUserResponse updateUser(@PathVariable("id") Long id, @RequestBody UpdateUserRequest request) {
+    userService.update(id, request.getName());
+    User user = userService.findUser(id);
+    return new UpdateUserResponse(user.getId(), user.getName());
+  }
+
+  @DeleteMapping("api/v1/users/{id}")
+  public void deleteUser(@PathVariable("id") Long id) {
+    userService.removeUser(id);
+  }
+
+  // #region Request & Response
+  @Data
+  @AllArgsConstructor
+  static class Result<T> {
+    private List<T> data;
+  }
+
+  @Data
+  static class CreateMemberRequest {
+    private String name;
   }
 
   @Data
@@ -44,4 +72,22 @@ public class UserApiController {
       this.id = id;
     }
   }
+
+  @Data
+  static class UpdateUserRequest {
+    private String name;
+  }
+
+  @Data
+  static class UpdateUserResponse {
+    private Long id;
+    private String name;
+
+    public UpdateUserResponse(Long id, String name) {
+      this.id = id;
+      this.name = name;
+    }
+
+  }
+  // #endregion
 }
--- server/shiftcracker/src/main/java/com/hanbal/shiftcracker/user/service/UserService.java
+++ server/shiftcracker/src/main/java/com/hanbal/shiftcracker/user/service/UserService.java
@@ -1,6 +1,7 @@
 package com.hanbal.shiftcracker.user.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.hanbal.shiftcracker.global.domain.User;
 import com.hanbal.shiftcracker.user.repository.UserRepository;
@@ -18,9 +19,33 @@ public class UserService {
     return userRepository.findAll();
   }
 
+  public User findUser(Long id) {
+    return userRepository.getById(id);
+  }
+
   public Long join(User user) {
     userRepository.save(user);
     return user.getId();
+  }
+
+  public void update(Long id, String name) {
+    Optional<User> result = userRepository.findById(id);
+    if (result.isPresent()) {
+      User user = result.get();
+      user.setName(name);
+    }
+  }
+
+  public void removeUser(Long id) {
+    Optional<User> findUser = userRepository.findById(id);
+    if (findUser.isPresent()) {
+      User user = findUser.get();
+
+      // 밸리데이션 처리
+
+      userRepository.delete(user);
+    }
+    userRepository.deleteById(id);
   }
 
 }
