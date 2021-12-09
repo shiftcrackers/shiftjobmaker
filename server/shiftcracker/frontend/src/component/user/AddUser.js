c2632ea4188880119d536b8007a0303a0a930ca9
YS.Bae <qodbtn41@naver.com>
2021-12-09 15:04:15 +0000

    회원가입 UI 변경

--- server/shiftcracker/frontend/src/component/user/AddUser.js
+++ server/shiftcracker/frontend/src/component/user/AddUser.js
@@ -1,21 +1,20 @@
 import React, { useState, useCallback } from 'react';
 import axios from 'axios';
 
-// import Avatar from '@mui/material/Avatar';
-// import Button from '@mui/material/Button';
-// import CssBaseline from '@mui/material/CssBaseline';
-// import TextField from '@mui/material/TextField';
-// import FormControlLabel from '@mui/material/FormControlLabel';
-// import Checkbox from '@mui/material/Checkbox';
-// import Link from '@mui/material/Link';
-// import Grid from '@mui/material/Grid';
-// import Box from '@mui/material/Box';
-// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-// import Typography from '@mui/material/Typography';
-// import Container from '@mui/material/Container';
-// import { ThemeProvider } from '@mui/material/styles';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 const AddUser = ({ history }) => {
+
+    const theme = createTheme();
     const [user, setUser] = useState({
         name: ''
     });
@@ -45,19 +44,54 @@ const AddUser = ({ history }) => {
     };
 
     return (
-        <div>
-            {/* <Typography component="h1" variant="h5">
-                Sign up
-            </Typography> */}
-            <h2>Add User</h2>
-            <form>
-                <div>
-                    <label>User Name</label>
-                    <input type="text" placeholder="Input your name" name="name" value={user.name} onChange={onChange} />
-                </div>
-                <button onClick={saveUser}>Save</button>
-            </form>
-        </div>
+        <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography component="h1" variant="h5">
+                        Sign up
+                    </Typography>
+                    <Box component="form" noValidate sx={{ mt: 3 }}>
+                        <Grid container >
+                            <Grid item xs={12}>
+                                <TextField
+                                    autoComplete="name"
+                                    value={user.name}
+                                    name="name"
+                                    required
+                                    fullWidth
+                                    id="name"
+                                    label="User Name"
+                                    autoFocus
+                                    onChange={onChange}
+                                />
+                            </Grid>
+
+
+                        </Grid>
+                        <Button
+                            // type="submit"
+                            fullWidth
+                            variant="contained"
+                            onClick={saveUser}
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Sign Up
+                        </Button>
+                    </Box>
+                </Box>
+            </Container>
+        </ThemeProvider>
     );
 };
 
