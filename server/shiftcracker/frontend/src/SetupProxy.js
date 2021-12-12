5443124647eea888de41c66d93ad696fff4d328f
qodbtn41 <qodbtn41@naver.com>
2021-12-13 01:23:23 +0900

    http-proxy-middleware 버전에 따른 수정 링크 참고 - https://fulladdr.tistory.com/7

--- server/shiftcracker/frontend/src/SetupProxy.js
+++ server/shiftcracker/frontend/src/SetupProxy.js
@@ -1,10 +1,12 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const proxy = require("http-proxy-middleware");
 
 module.exports = function (app) {
-    app.use(
-        createProxyMiddleware('/api', {
-            target: 'http://146.56.96.107:8080/',
-            changeOrigin: true
-        })
-    )
+  app.use(
+    "/api",
+    proxy({
+      // target: "http://127.0.0.1:8080/",
+      target: "http://146.56.96.107:8080/",
+      changeOrigin: true,
+    })
+  );
 };
